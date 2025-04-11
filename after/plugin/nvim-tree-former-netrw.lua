28e489652a285a7b4b50f89ca038c2ec378d9b62
Iurii Pavlikov <fesst.spb@gmail.com>
2025-04-11 05:04:50 +0100

    improve nvim-tree experience by excluding unnecessary folder caches significant amount of data

--- after/plugin/nvim-tree-former-netrw.lua
+++ after/plugin/nvim-tree-former-netrw.lua
@@ -11,7 +11,22 @@ require("nvim-tree").setup {
 		group_empty = true,
 	},
 	filters = {
-		dotfiles = false,--true,
+		dotfiles = false,
+		custom = {
+			'\\.git', 'node_modules', '.cache',
+			'Applications', 'Library', 'MusicCreation',
+			'\\.aspnet', '\\.colima', '\\.local', '\\.npm', '\\.nvm',
+			'\\.oh-my-zsh', '\\.venv', '\\.docker', 'Movies', 'Public',
+			'Pictures', 'Desktop', 'Documents', 'Downloads',
+			'Live Projects', 'Live project mac', '010-installers',
+			'\\.zsh_sessions', 'Push3',
+			'\\.CFUserTextEncoding', '\\.DS_Store', '\\.lesshst',
+			'\\.Spotlight-V100','\\.node_repl_history',
+			'\\.python_history', '\\.sdkman','\\.templateengine',
+			'\\.mounty', '\\.nuget', '\\.dotnet', '\\.vim',
+			'\\.vscode', '\\.idead', '\\.ssh', '\\.tmux',
+			'\\.w3m', '\\.cursor'
+		}
 	}
 }
 vim.api.nvim_set_keymap("n", "<leader>pv", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
