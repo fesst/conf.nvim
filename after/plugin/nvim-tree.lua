31c38fe7f4206ab8922c4e0e72a4d5ab9e459092
Iurii Pavlikov <fesst.spb@gmail.com>
2025-05-20 05:30:22 +0100

    Consolidate files

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -2,8 +2,34 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- Function to open nvim-tree on directory open
+local function open_nvim_tree(data)
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not directory then
+        return
+    end
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 -- Nvim-tree configuration
 require("nvim-tree").setup({
+    sync_root_with_cwd = true,
+    reload_on_bufenter = true,
+    hijack_directories = {
+        enable = false,
+    },
+    sort = {
+        sorter = "case_sensitive",
+    },
     view = {
         width = 30,
     },
@@ -12,8 +38,52 @@ require("nvim-tree").setup({
     },
     filters = {
         dotfiles = false,
+        custom = {
+            "010-installers",
+            "Applications",
+            "Desktop",
+            "Documents",
+            "Downloads",
+            "Library",
+            "Live projects mac",
+            "Live\\ projects",
+            "Movies",
+            "Music",
+            "MusicCreation",
+            "Pictures",
+            "Public",
+            "Push3",
+            "\\.CFUserTextEncoding",
+            "\\.DS_Store",
+            "\\.Spotlight-V100",
+            "\\.node_repl_history",
+            "\\.aspnet",
+            "\\.cache",
+            "\\.colima",
+            "\\.docker",
+            "\\.dotnet",
+            "\\.git",
+            "\\.idead",
+            "\\.lesshst",
+            "\\.local",
+            "\\.mounty",
+            "\\.npm",
+            "\\.nuget",
+            "\\.nvm",
+            "\\.oh-my-zsh",
+            "\\.python_history",
+            "\\.sdkman",
+            "\\.templateengine",
+            "\\.ssh",
+            "\\.tmux",
+            "\\.venv",
+            "\\.vim",
+            "\\.vscode",
+            "\\.w3m",
+            "\\.zsh_sessions",
+            "node_modules",
+        },
     },
-    -- Additional settings from former netrw configuration
     git = {
         enable = true,
     },
@@ -32,9 +102,14 @@ require("nvim-tree").setup({
     },
     actions = {
         open_file = {
+            quit_on_open = true,
             window_picker = {
                 enable = true,
             },
         },
     },
-}) 
+})
+
+-- Key mappings
+vim.api.nvim_set_keymap("n", "<leader>pv", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>p-", ":NvimTreeToggle<CR>", { noremap = true, silent = true }) 
